f4e92b25fc7ce39b17372cf111d8fc7edf66e605
Schwayzer <furkann.onay@gmail.com>
2023-03-16 16:30:49 +0300

    admin panel components created

--- NewBlogApp/blog-frontend/components/Comments.jsx
+++ NewBlogApp/blog-frontend/components/Comments.jsx
@@ -1,47 +1,46 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Comments = ({ specialId }) => {
   /*axios ile yorumlar burada çekilecek ve aşağıda axios ile map edilip yayınlanacak */
-  // const url = "http://localhost:3000/api/comment/";
-  // useEffect(() => {
-  //   axios.get(url).then((res) => {
-  //     console.log(res, "commentres");
-  //     const filteredComments = res.data.filter((item) => {
-  //       console.log(specialId, "ResPostId");
-  //       return item.postId === specialId;
-  //     });
-  //     console.log(filteredComments, "filtered comments");
-  //   });
-  // }, [specialId]);
+  const url = "http://localhost:3000/api/comment/";
+  const [filteredComments, setFilteredComments] = useState([]);
 
-  return (
-    <article className="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900">
-      <footer className="flex justify-between items-center mb-2">
-        <div className="flex items-center">
-          <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
-            <img
-              className="mr-2 w-6 h-6 rounded-full"
-              src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
-              alt="Michael Gough"
-            />
-            Michael Gough
-          </p>
-          <p className="text-sm text-gray-600 dark:text-gray-400">
-            <time pubdate dateTime="2022-02-08" title="February 8th, 2022">
-              Feb. 8, 2022
-            </time>
-          </p>
-        </div>
-      </footer>
-      <p className="text-gray-500 dark:text-gray-400">
-        Very straight-to-point article. Really worth time reading. Thank you!
-        But tools are just the instruments for the UX designers. The knowledge
-        of the design tools are as important as the creation of the design
-        strategy.
-      </p>
-    </article>
-  );
+  useEffect(() => {
+    axios.get(url).then((res) => {
+      console.log(res, "commentres");
+      const comments = res.data.filter((item) => item.postId == specialId);
+      setFilteredComments(comments);
+    });
+  }, [specialId]);
+  console.log(filteredComments, "filtered");
+  {
+    filteredComments.map((item) => {
+      console.log(item);
+      return (
+        <article className="p-6 mb-6 text-base bg-white rounded-lg dark:bg-gray-900">
+          <footer className="flex justify-between items-center mb-2">
+            <div className="flex items-center">
+              <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+                <img
+                  className="mr-2 w-6 h-6 rounded-full"
+                  src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
+                  alt="Michael Gough"
+                />
+                {item.authorId}
+              </p>
+              <p className="text-sm text-gray-600 dark:text-gray-400">
+                <time pubdate dateTime="2022-02-08" title="February 8th, 2022">
+                  {item.createDate}
+                </time>
+              </p>
+            </div>
+          </footer>
+          <p className="text-gray-500 dark:text-gray-400">{item.text}</p>
+        </article>
+      );
+    });
+  }
 };
 
 export default Comments;
--- NewBlogApp/blog-frontend/pages/adminDashboard.js
+++ NewBlogApp/blog-frontend/pages/adminDashboard.js
@@ -1,7 +1,20 @@
+import DashboardComment from "@/components/DashboardComment";
+import DashboardPost from "@/components/DashboardPost";
+import DashboardUser from "@/components/DashboardUser";
 import React from "react";
 
 const adminDashboard = () => {
-  return <div className="height">adminDashboard</div>;
+  return (
+    <div className="height">
+      <h1 className="text-center text-5xl">DashBoard</h1>
+      <h1 className="text-center text-2xl mt-5">Users</h1>
+      <DashboardUser />
+      <h1 className="text-center text-2xl mt-5">Comments</h1>
+      <DashboardComment />
+      <h1 className="text-center text-2xl mt-5">Posts</h1>
+      <DashboardPost />
+    </div>
+  );
 };
 
 export default adminDashboard;
